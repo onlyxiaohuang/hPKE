1b6da0e166638182ba3efa06ddc580ac04d3a2b7
olxh <1030263320@qq.com>
2023-12-29 01:45:13 -0800

    my work on lab4_3 is done.4

--- kernel/rfs.c
+++ kernel/rfs.c
@@ -596,7 +596,12 @@ int rfs_link(struct vinode *parent, struct dentry *sub_dentry, struct vinode *li
   //    rfs_add_direntry here.
   // 3) persistent the changes to disk. you can use rfs_write_back_vinode here.
   //
-  panic("You need to implement the code for creating a hard link in lab4_3.\n" );
+  //panic("You need to implement the code for creating a hard link in lab4_3.\n" );
+  link_node -> nlinks ++;
+  if(rfs_add_direntry(parent, sub_dentry -> name,link_node -> inum) == -1 || rfs_write_back_vinode(link_node) == -1)
+    return -1;
+  
+  return 0;
 }
 
 //
